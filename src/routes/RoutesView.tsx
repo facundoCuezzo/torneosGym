88f4c027d0886fc7ad812a579c75b92fa2a83e4d
SantiJaime <santij03@gmail.com>
2025-05-26 18:45:38 -0300

    creo registerpage y muevo form a formcomp, agrego esquema de validacion de register y logo de proyecto

--- src/routes/RoutesView.tsx
+++ src/routes/RoutesView.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from '../pages/LoginPage';
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const RoutesView = () => {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage/>} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
   );
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,3 +2,8 @@ interface LoginFormData {
   email: string;
   password: string;
 }
+interface RegisterFormData {
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
