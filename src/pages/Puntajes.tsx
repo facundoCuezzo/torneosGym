4966013ed2b21da7f1df04ed4403fd70c9b9ebb4
Facundo Cuezzo <facuu.cuezzo@gmail.com>
2025-07-05 16:21:51 -0300

    actualizacion puntajes

--- src/pages/Puntajes.tsx
+++ src/pages/Puntajes.tsx
@@ -1,28 +1,69 @@
-import Dropdown from "react-bootstrap/Dropdown";
+import SelectTournamentComp from "../components/SelectTournamentComp";
 import useUsers from "../hooks/useUsers";
 import CardComp from "../components/CardComp";
+import useTournaments from "../hooks/useTournaments";
+import { Spinner } from "react-bootstrap";
+import FilterComp from "../components/FilterComp";
 
 export default function Puntajes() {
   const { user } = useUsers();
+  const {
+    tournaments,
+    selectedTournament,
+    setSelectedTournament,
+    loading,
+    membersTournaments
+  } = useTournaments();
+
   return (
-    <div
-      className="d-flex justify-content-center align-items-start pt-5"
-      style={{ minHeight: "60vh" }}
-    >
+    <div className="d-flex flex-column align-items-center pt-5" style={{ minHeight: "60vh" }}>
+     
       <CardComp user={user} color="light" textColor="dark">
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Seleccione el Torneo
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="#">Action</Dropdown.Item>
-            <Dropdown.Item href="#">Another action</Dropdown.Item>
-            <Dropdown.Item href="#">Something else here</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item href="#">Separated link</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        {loading ? (
+          <div className="d-flex justify-content-center gap-1">
+            <Spinner animation="border" variant="light" size="sm" />
+            <h6 className="text-white">Cargando...</h6>
+          </div>
+        ) : !tournaments || tournaments.length === 0 ? (
+          <p>No hay torneos disponibles</p>
+        ) : (
+          <SelectTournamentComp
+            selectedTournament={selectedTournament}
+            setSelectedTournament={setSelectedTournament}
+            tournaments={tournaments || []}
+          />
+        )}
       </CardComp>
+  
+      {selectedTournament !== 0 && (
+        <div className="mt-3 w-100" style={{ maxWidth: 600 }}>
+          <FilterComp submitFilter={(filtro) => {
+            console.log("Filtro enviado:", filtro);
+          }} />
+           <table className="table mt-3">
+      <thead>
+        <tr>
+          <th>DNI</th>
+          <th>Nombre</th>
+          <th>Categoría</th>
+          <th>Nivel</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Reemplaza esto por el array filtrado si tienes lógica de filtro */}
+        {membersTournaments && membersTournaments.map((alumno) => (
+          <tr key={alumno.id_member}>
+            <td>{alumno.dni}</td>
+            <td>{alumno.gym}</td>
+            <td>{alumno.member}</td>
+            <td>{alumno.id_gym}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+        </div>
+      )}
+    
     </div>
   );
 }
